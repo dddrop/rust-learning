2ad351cd6f0d1fa1b20bc99860dbd353042fa31a
dddrop <go@dddrop.dev>
2024-05-27 09:57:00 +0900

    feat: More Math!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
 fn main() {
-    let name = "dddrop";
-    println!("Hello, My name is {}.", name);
-
-    let apples = 10;
-    println!("I have {} apples.", apples);
-    
-    let buy_more_apples = 5;
-    println!("I will buy {} more apples.", buy_more_apples);
-    
-    let total_apples = apples + buy_more_apples;
-    println!("I will have {} apples in total.", total_apples);
+    let answer = 6 / 2 + 4 * 3;
+    println!("The answer is: {}", answer);
 }
