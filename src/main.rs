87b5836dcc8ee871012b10fd812da19efe00c926
dddrop <go@dddrop.dev>
2024-05-27 12:48:32 +0900

    feat: While

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-fn fact(x: u32) -> u32 {
-    if x == 0 {
-        1
-    } else {
-        x * fact(x - 1)
-    }
-}
-
 fn main() {
-    println!("Fact of 5: {}", fact(5));
+    let mut keep_going = true;
+    while keep_going {
+        println!("Inside the while loop");
+        keep_going = false;
+    }
 }
