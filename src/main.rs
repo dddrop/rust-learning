55437a41d31098836074f72fd110e33f290d2710
dddrop <go@dddrop.dev>
2024-05-27 10:42:11 +0900

    feat: If

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 fn main() {
-    let x = true;
-    let y = !x; // not x
-    let z = !y; // not y
-
-    // All three of the following do the same thing
-    assert!(z == true);
-    assert_eq!(z, true);
-    assert!(z);
-
-    // Same for these three
-    assert!(y == false);
-    assert_eq!(y, false);
-    assert!(!y);
-
-    println!("Woohoo!");
+    let temp = 20;
+    if temp > 15 && temp < 27 {
+        println!("It's fairly comfortable in here!");
+    }
 }
