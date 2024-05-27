b0e6c31a4f08850597da6e26e8e5dd62895041a4
dddrop <go@dddrop.dev>
2024-05-27 13:55:50 +0900

    feat: Static Methods

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,20 @@ struct Fruit {
 }
 
 impl Fruit {
-    fn price(self) -> i32 {
+    fn price(&self) -> i32 {
         self.apples * 8 + self.bananas * 12
     }
-}
 
-fn new_fruit() -> Fruit {
-    Fruit {
-        apples: 10,
-        bananas: 5,
+    fn new() -> Fruit {
+        Fruit {
+            apples: 10,
+            bananas: 5,
+        }
     }
 }
 
 fn main() {
-    let fruit = new_fruit();
+    let fruit = Fruit::new();
     let price = fruit.price();
     println!("Price is {}", price);
 }
