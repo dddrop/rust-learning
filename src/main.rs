a18306de2fc65d04d38e12044c503b8a21d4b9a1
dddrop <go@dddrop.dev>
2024-05-27 13:51:50 +0900

    feat: Method Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Fruit {
+    apples: i32,
+    bananas: i32,
+}
+
+impl Fruit {
+    fn price(self) -> i32 {
+        self.apples * 8 + self.bananas * 12
+    }
+}
+
+fn new_fruit() -> Fruit {
+    Fruit {
+        apples: 10,
+        bananas: 5,
+    }
+}
+
 fn main() {
-    println!("x == {}", -5_i32.abs());
+    let fruit = new_fruit();
+    let price = fruit.price();
+    println!("Price is {}", price);
 }
