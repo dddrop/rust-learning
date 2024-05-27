5e0ec4f2b458f3b04aedff3878bcbc6ffb9591ec
dddrop <go@dddrop.dev>
2024-05-27 09:56:16 +0900

    feat: Variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, My name is {}.", "dddrop");
+    let name = "dddrop";
+    println!("Hello, My name is {}.", name);
+
+    let apples = 10;
+    println!("I have {} apples.", apples);
+    
+    let buy_more_apples = 5;
+    println!("I will buy {} more apples.", buy_more_apples);
+    
+    let total_apples = apples + buy_more_apples;
+    println!("I will have {} apples in total.", total_apples);
 }
