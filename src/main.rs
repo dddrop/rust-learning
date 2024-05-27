af5247fbcf9c080125e0b85c2abcb404e149f0c0
dddrop <go@dddrop.dev>
2024-05-27 10:15:30 +0900

    feat: Comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    let name: &str = "Michael";
-    println!("My name is {}", name);
+fn main(/* hey, I can do this! */) {
+    // This line is entirely ignored
+    println!("Hello, world!"); // This printed a message
+                               // All done, bye!
+
+    /* This line is entirely ignored */
+    println!("Hello, world!" /* sneaky comment */);
+    /* All done, bye!
+      I had a great time programming with you.
+    */
 }
