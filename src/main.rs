360664fff9e828d7749e9a0a91661053af203be3
dddrop <go@dddrop.dev>
2024-05-27 10:00:45 +0900

    feat: Types

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
 fn main() {
-    let x = 5;
-    let y = 10;
-    let name = "Michael";
-    println!("My name is {}, and the answer is {}", name, x + y);
-
-    let x = 5 + 3;
-    let x = x * 2;
-    let x = x - 6;
-    let x = x / 2;
-    println!("The answer is {}", x);
+    let name: &str = "Michael";
+    println!("My name is {}", name);
 }
