1a6cbd12c28376aa212a963726b837b74ac9567f
dddrop <go@dddrop.dev>
2024-05-27 10:40:16 +0900

    feat: Functions Returning Booleans

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
+fn temperature() -> i32 {
+    15 // feel free to change this!
+}
+
+fn is_hot() -> bool {
+    temperature() > 30
+}
+
 fn main() {
-    // These two things are basically the same
-    assert!("apple" != "banana");
-    assert_ne!("apple", "banana");
-
-    assert!(5 == 5);
-    assert_eq!(5, 5);
-
-    assert!(() == ());
-
-    // Oh, did I mention that we can use true and false like this?
-    // They're also literals!
-    assert!(true == true);
-    assert!(true != false);
-
-    assert!(6 != 3);
-    assert!(6 > 3);
-    assert!(3 < 6);
-    assert!(6 >= 3);
-
+    assert!(is_hot() == false);
     println!("Success!");
 }
