e2bad4e896a9cc8a902a533c674b27a9d06066e5
dddrop <go@dddrop.dev>
2024-05-27 13:41:03 +0900

    feat: Single Mutable Reference

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-struct Fruit {
-    apples: i32,
-    bananas: i32,
-}
-
-fn increase_fruit(mut fruit: Fruit) -> Fruit {
-    fruit.apples *= 2;
-    fruit.bananas *= 3;
-    fruit
-}
-
-fn print_fruit(fruit: Fruit) -> Fruit {
-    println!(
-        "You have {} apples and {} bananas",
-        fruit.apples, fruit.bananas
-    );
-    fruit
-}
-
 fn main() {
-    let fruit = Fruit {
-        apples: 10,
-        bananas: 5,
-    };
+    let mut x = 5;
 
-    let fruit = print_fruit(fruit);
-    let fruit = increase_fruit(fruit);
-    print_fruit(fruit);
+    let y = &mut x; // freeze
+    *y *= 2; // unfreeze
+
+    println!("x == {}", x);
 }
