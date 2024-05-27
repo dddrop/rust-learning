4130ca8138b83674465ca70ee55fe4ff2ad0440c
dddrop <go@dddrop.dev>
2024-05-27 09:59:18 +0900

    feat: Multiple Variables and Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let answer = 6 / 2 + 4 * 3;
-    println!("The answer is: {}", answer);
+    let x = 5;
+    let y = 10;
+    let name = "Michael";
+    println!("My name is {}, and the answer is {}", name, x + y);
+
+    let x = 5 + 3;
+    let x = x * 2;
+    let x = x - 6;
+    let x = x / 2;
+    println!("The answer is {}", x);
 }
