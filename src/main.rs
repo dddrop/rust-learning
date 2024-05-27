5ca5c501f96e377921f023da70b7d8a22da38d69
dddrop <go@dddrop.dev>
2024-05-27 10:29:59 +0900

    feat: Variable and Parameter Scope

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-fn eat(count: i32, food: &str) {
-    println!("You ate {} helpings of {}", count, food);
-}
-
 fn main() {
-    eat(5, "apples");
-    eat(8, "bananas");
+    let apples = 10; // apples is 10
+    println!("apples == {}", apples);
+    {
+        let apples = 20; // shadow apples in this scope
+        println!("apples == {}", apples);
+    }
+    // That block's scope is done
+    // Now our original apples is back in scope
+    // What do you think this will output?
+    println!("apples == {}", apples);
 }
