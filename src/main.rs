3093f749e03f54cc3e66d1cf6f1a0dacc0a42690
dddrop <go@dddrop.dev>
2024-05-29 08:32:13 +0900

    feat: Owned Strings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let name: &'static str = "Michael";
-    println!("Name is {}", name);
+    let first_name = "Michael";
+    let last_name = " Snoyman";
+    let full_name = first_name.to_owned() + last_name;
+    println!("Full name is {}", full_name);
 }
