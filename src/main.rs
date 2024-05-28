3a39abe4541d106d38f23a6e8a0504d0bb16e0ab
dddrop <go@dddrop.dev>
2024-05-29 08:50:45 +0900

    feat: Slicing a Slice

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
-    let numbers: [i32; 5] = [2, 3, 8, 1, 9];
-    let mut i = 0;
+    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    while i < numbers.len() {
-        println!("numbers[{}] == {}", i, numbers[i]);
-        i += 1;
-    }
+    println!("{:?}", &numbers[1..4]);
 }
