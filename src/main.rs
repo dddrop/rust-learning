069842c20bc98edaa5675d2b0eb4153406277aca
dddrop <go@dddrop.dev>
2024-05-29 08:54:29 +0900

    feat: Traits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-fn main() {
-    let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+trait Double {
+    fn double(&self) -> Self;
+}
 
-    println!("{:?}", &numbers[1..4]);
+impl Double for i32 {
+    fn double(&self) -> Self {
+        self * 2
+    }
+}
+
+impl Double for i64 {
+    fn double(&self) -> Self {
+        self * 2
+    }
+}
+
+fn main() {
+    println!("double 5_i32 == {}", 5_i32.double());
+    println!("double 5_i64 == {}", 5_i64.double());
 }
