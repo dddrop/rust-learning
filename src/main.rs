b9dcb97414ff9c5e30e9b2a385e30ea34831f68e
dddrop <go@dddrop.dev>
2024-05-29 08:26:37 +0900

    feat: Lifetimes of String Literals

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,4 @@
-struct Fruit {
-    apples: i32,
-    bananas: i32,
-}
-
-impl Fruit {
-    fn new() -> Fruit {
-        Fruit {
-            apples: 10,
-            bananas: 5,
-        }
-    }
-
-    fn increase(mut self) -> Self {
-        self.apples *= 2;
-        self.bananas *= 3;
-        self
-    }
-
-    fn print(&self) {
-        println!(
-            "You have {} apples and {} bananas",
-            self.apples, self.bananas
-        );
-    }
-}
-
 fn main() {
-    Fruit::new().increase().print();
+    let name: &'static str = "Michael";
+    println!("Name is {}", name);
 }
