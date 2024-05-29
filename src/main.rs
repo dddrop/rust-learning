0c1dc35ae13277a9176828d37840747ae2fa6fc9
dddrop <go@dddrop.dev>
2024-05-29 09:11:27 +0900

    feat: Copy and Marker Traits

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Fruit<T> {
     apples: T,
     bananas: T,
 }
 
-fn print_fruit(fruit: Fruit<i32>) {
-    println!("Apples: {}, bananas: {}", fruit.apples, fruit.bananas);
-}
-
-fn main() {
-    let mut fruit = Fruit {
-        apples: 5,
-        bananas: 10,
-    };
-    print_fruit(fruit.clone());
-    fruit.apples *= 2;
-    fruit.bananas *= 3;
-    print_fruit(fruit);
-}
+fn main() {}
