d849664c021a84afe7ea9fa5baabfe0e9331e21f
dddrop <go@dddrop.dev>
2024-05-29 09:19:07 +0900

    feat: Multiple Bounds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use std::ops::Add;
+trait Double {
+    fn double(self) -> Self;
+}
+
+impl Double for i32 {
+    fn double(self) -> i32 {
+        self * 2
+    }
+}
+
+fn info<T>(x: T)
+where
+    T: Double + std::fmt::Display + Copy,
+{
+    println!("Original number: {}", x);
+    println!("Doubled number: {}", x.double());
+    println!("Quadrupled number: {}", x.double().double());
+}
+
 fn main() {
-    let x = 2_i32.add(3);
-    println!("x == {}", x);
+    info(5);
 }
