4a4150d45e64418f322b2158fdaaa5bea1e67f4b
dddrop <go@dddrop.dev>
2024-05-29 09:48:17 +0900

    feat: Iterating Over a Vec

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-fn main() {
-    for i in 1..11 {
-        println!("i == {}", i);
+struct VecIter {
+    vec: Vec<u32>,
+    index: usize,
+}
+
+impl Iterator for VecIter {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        if self.index >= self.vec.len() {
+            None
+        } else {
+            let res = Some(self.vec[self.index]);
+            self.index += 1;
+            res
+        }
     }
-    for i in 1..=10 {
-        println!("i == {}", i);
+}
+
+fn main() {
+    let fibs: Vec<u32> = vec![1, 1, 2, 3, 5, 8, 13];
+    let iter = VecIter {
+        vec: fibs,
+        index: 0,
+    };
+
+    for x in iter {
+        println!("{}", x);
     }
 }
