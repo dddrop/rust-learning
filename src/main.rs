05ee1602295040e6a0d575ef621be8f7ffcbbcf6
dddrop <go@dddrop.dev>
2024-05-29 09:50:45 +0900

    feat: The Rule of 3

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
-struct VecIter {
-    vec: Vec<u32>,
-    index: usize,
-}
-
-impl Iterator for VecIter {
-    type Item = u32;
-    fn next(&mut self) -> Option<u32> {
-        if self.index >= self.vec.len() {
-            None
-        } else {
-            let res = Some(self.vec[self.index]);
-            self.index += 1;
-            res
-        }
-    }
-}
-
 fn main() {
-    let fibs: Vec<u32> = vec![1, 1, 2, 3, 5, 8, 13];
-    let iter = VecIter {
-        vec: fibs,
-        index: 0,
-    };
+    let mut fibs = vec![1, 1, 2, 3, 5, 8, 13];
 
-    for x in iter {
+    for x in fibs.iter_mut() {
         println!("{}", x);
+        *x *= 2;
     }
+
+    println!("{:?}", fibs);
 }
